939e32f50bfb5f5d6750c302c2fcd60ed8591e02
Genji <genji-fujimori@cybozu.co.jp>
2023-08-16 17:48:02 +0900

    Added body

--- Garoon_to_Google/Garoon_to_Google_Bookmarklet_v1.js
+++ Garoon_to_Google/Garoon_to_Google_Bookmarklet_v1.js
@@ -1,32 +1,27 @@
 javascript: (() => {
   const formatTimestamp = (dateString) =>
-    new Date(dateString).toISOString().replaceAll(/[-:]|\.\d+/g, "");
+    new Date(dateString).toISOString().replaceAll(/[-:]|\.\d+/g, '');
+  const bodyFormat = (inputText) => inputText.replace(/\n/g, '<br>');
   const addCalendar = (event) => {
     console.log({ event });
-    const params = new URLSearchParams({ action: "TEMPLATE" });
-
-    params.set("text", event.subject);
-
     const start = formatTimestamp(event.start.dateTime);
     const end = formatTimestamp(event.end.dateTime);
-    params.set("dates", `${start}/${end}`);
-
-    const origin = location.origin.replace(".s.", ".");
+    const origin = location.origin.replace('.s.', '.');
     const url = `${origin}${location.pathname}?event=${event.id}`;
-
-    params.set("details", url);
-
+    const body = bodyFormat(event.notes);
+    const params = new URLSearchParams({ action: 'TEMPLATE' });
+    params.set('dates', `${start}/${end}`);
+    params.set('text', event.subject);
+    params.set('location', url);
+    params.set('details', body);
     open(`https://www.google.com/calendar/render?${params.toString()}`);
   };
-
   const event = window.garoon?.schedule?.event?.get();
-
   if (event === undefined) {
     alert(
-      "Error: Not on a Garoon schedule.\nPlease open a specific Garoon event."
+      `Error: Not on a Garoon event.\nPlease open a specific Garoon event.`
     );
     return;
   }
-
   addCalendar(event);
 })();
