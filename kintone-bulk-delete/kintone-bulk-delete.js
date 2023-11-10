2854cfe85fc0bf39fd50a992a97ea7a4b14a3673
Genji <genji-fujimori@cybozu.co.jp>
2023-11-10 18:47:49 +0900

    v4 - working

--- kintone-bulk-delete/kintone-bulk-delete.js
+++ kintone-bulk-delete/kintone-bulk-delete.js
@@ -19,59 +19,37 @@ javascript: (function () {
     } while (!confirm(`${userInput} is not a valid number.\nClick "OK" to try again.\nClick "Cancel" to cancel the operation.`));
   };
   const appId = window.kintone?.app?.getId();
-  const errMsg = `Error: This is not a Kintone App.\nOpen a specific Kintone App when executing this bookmarklet.`;
-  if (appId === undefined) {
-    alert(errMsg);
-    console.log(errMsg);
-    return;
+  if (typeof appId !== 'number') {
+    return logAndAlert(`Error: This is not a Kintone App.\nOpen a specific Kintone App when executing this bookmarklet.`);
   }
-  console.log(`App ID: ${appId}`);
   const deleteStart = askForNumber('First record to delete:');
   if (deleteStart === undefined) return;
-
   const deleteEnd = askForNumber('Last record to delete:');
   if (deleteEnd === undefined) return;
   if (deleteEnd < deleteStart) {
-    return alert('The last record cannot be before the start record. Deletion cancelled.');
+    return logAndAlert('The last record cannot be before the start record. Deletion cancelled.', true, false);
   }
-
-  let deleteCount = 0;
-  let deleteArray = [];
   const counterBody = {
     'app': appId,
     'query': `$id >= "${deleteStart}" and $id <= "${deleteEnd}" order by $id asc`,
     'fields': ['$id']
   };
   kintone.api(kintone.api.url('/k/v1/records.json', true), 'GET', counterBody, function (resp) {
-    deleteCount = resp.records.length;
-    console.log(resp);
-    if (resp.records && Array.isArray(resp.records)) {
-      deleteArray = resp.records.map(record => {
-        if (record.$id && record.$id.value) {
-          return record.$id.value;
-        }
-        return null;
-      }).filter(id => id !== null);
+    const deleteArray = resp.records.map(record => record.$id?.value).filter(Boolean);
+    if (deleteArray.length === 0) {
+      return logAndAlert('No records were deleted.', false);
     }
-    const userConfirmation = window.confirm(`Records from ${deleteStart} to ${deleteEnd} will be deleted in this Kintone App (App ID: ${appId}).\n${deleteCount} records in total.\n\n Click "OK" to delete the records.\nClick "Cancel" to cancel the operation.`);
-    if (!userConfirmation) {
-      console.log(userCancel);
-      alert(recNotDeleted);
-      return;
-    }
-    console.log('deleteArray');
-    console.log(deleteArray);
+    const userConfirmation = window.confirm(`Records from ${deleteStart} to ${deleteEnd} will be deleted in this Kintone App (App ID: ${appId}).\n${deleteArray.length} records in total.\n\n Click "OK" to delete the records.\nClick "Cancel" to cancel the operation.`);
+    if (!userConfirmation) return logAndAlert('Deletion cancelled by user.');
     const deleteBody = { 'app': appId, 'ids': deleteArray };
     const deleteMsg = `Records from ${deleteStart} to ${deleteEnd} has been deleted.\nRefresh the page to see the changes.`;
     kintone.api(kintone.api.url('/k/v1/records', true), 'DELETE', deleteBody, (resp) => {
-      console.log(deleteMsg);
-      alert(deleteMsg);
+      logAndAlert(deleteMsg);
     }, (error) => {
-      console.log(recNotDeleted);
-      console.log(error);
-      alert(`Error! ${recNotDeleted}\nCheck the console for more details.`);
+      logAndAlert(`Error! No records were deleted. Check the console for details.`, true, false);
+      console.error(error);
     });
   }, function (error) {
-    console.log(error);
+    console.error(error);
   });
 })();
