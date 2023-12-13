3e0cc4eeb17e1bf6d69418129827406ad077f5dc
Genji <genji-fujimori@cybozu.co.jp>
2023-12-13 16:22:27 +0900

    working version 💪

---
+++ kintone-tool-app-field-code-table/dev-notes/v4-output.md
@@ -0,0 +1,11 @@
+| Field Name       | Code             | Type             |
+| ---------------- | ---------------- | ---------------- |
+| Article Title    | Text             | SINGLE_LINE_TEXT |
+| Content          | Text_area        | MULTI_LINE_TEXT  |
+| Created by       | Created_by       | CREATOR          |
+| Created datetime | Created_datetime | CREATED_TIME     |
+| Link             | Text_0           | SINGLE_LINE_TEXT |
+| Record number    | Record_number    | RECORD_NUMBER    |
+| Updated by       | Updated_by       | MODIFIER         |
+| Updated datetime | Updated_datetime | UPDATED_TIME     |
+| Written By       | Drop_down        | DROP_DOWN        |
---
+++ kintone-tool-app-field-code-table/dev-notes/v4-sorted.js
@@ -0,0 +1,141 @@
+javascript: (() => {
+  function markdownTableFormatter() {
+    let cells = [];
+    let columnWidths = [];
+    let outputTable = "";
+
+    function addMissingCellColumns() {
+      for (let row_i = 0, row_l = cells.length; row_i < row_l; row_i++) {
+        for (let col_i = 0, col_l = columnWidths.length; col_i < col_l; col_i++) {
+          if (typeof cells[row_i][col_i] === 'undefined') {
+            cells[row_i][col_i] = '';
+          }
+        }
+      }
+    }
+
+    function getColumnWidths() {
+      columnWidths = [];
+
+      for (let row_i = 0, row_l = cells.length; row_i < row_l; row_i++) {
+        for (let col_i = 0, col_l = cells[row_i].length; col_i < col_l; col_i++) {
+          if (typeof columnWidths[col_i] === 'undefined') {
+            columnWidths[col_i] = cells[row_i][col_i].length;
+          } else if (columnWidths[col_i] < cells[row_i][col_i].length) {
+            columnWidths[col_i] = cells[row_i][col_i].length;
+          }
+        }
+      }
+    }
+
+    function importTable(table) {
+      let tableRows = table.split("\n");
+
+      while (tableRows[0].indexOf('|') === -1) {
+        tableRows.shift();
+      }
+
+      for (let row_i = 0, row_l = tableRows.length; row_i < row_l; row_i++) {
+        if (tableRows[row_i].indexOf('|') === -1) {
+          continue;
+        }
+
+        cells[row_i] = [];
+        let rowColumns = tableRows[row_i].split(/(?<!\\)\|/g);
+
+        for (let col_i = 0, col_l = rowColumns.length; col_i < col_l; col_i++) {
+          let cell = rowColumns[col_i].trim();
+          if (row_i === 1) {
+            cell = cell.replace(/-+/g, "-");
+          }
+          cells[row_i][col_i] = cell;
+        }
+      }
+
+      getColumnWidths();
+
+      trimCells();
+    }
+
+    function padCellsForOutput() {
+      for (let row_i = 0, row_l = cells.length; row_i < row_l; row_i++) {
+        for (let col_i = 0, col_l = cells[row_i].length; col_i < col_l; col_i++) {
+          let paddingChar = row_i === 1 ? '-' : ' ';
+          while (cells[row_i][col_i].length < columnWidths[col_i]) {
+            cells[row_i][col_i] += paddingChar;
+          }
+        }
+      }
+    }
+
+    function trimCells() {
+      if (columnWidths[0] === 0) {
+        cells.forEach(row => row.shift());
+      }
+
+      getColumnWidths();
+
+      if (columnWidths[columnWidths.length - 1] === 0) {
+        cells.forEach(row => {
+          if (row.length === columnWidths.length) {
+            row.pop();
+          }
+        });
+      }
+
+      getColumnWidths();
+    }
+
+    return {
+      formatTable: function (table) {
+        importTable(table);
+        addMissingCellColumns();
+        padCellsForOutput();
+
+        outputTable = "| " + cells[0].join(" | ") + " |\n";
+
+        outputTable += "|-" + cells[1].join("-|-") + "-|\n";
+
+        for (let row_i = 2, row_l = cells.length; row_i < row_l; row_i++) {
+          outputTable += "| " + cells[row_i].join(" | ") + " |\n";
+        }
+
+        return outputTable;
+      }
+    };
+  }
+
+  const body = { 'app': kintone.app.getId() };
+
+  kintone.api(kintone.api.url('/k/v1/app/form/fields', true), 'GET', body, function (resp) {
+    extractData(resp.properties);
+    console.log(resp.properties);
+  }, function (error) {
+    console.log(error);
+  });
+
+  function extractData(rawProperties) {
+    const mdTableHeader = `| Field Name | Code | Type |\n| ---------- | ---------- | --------------- |`;
+    const mdRows = [];
+
+    for (const field in rawProperties) {
+      const fieldJson = rawProperties[field];
+      const type = fieldJson.type;
+      const label = fieldJson.label;
+      const code = fieldJson.code;
+      if (fieldJson.hasOwnProperty('enabled')) {
+        if (fieldJson.enabled === true) {
+          mdRows.push(`| ${label} | ${code} | ${type} |`);
+        }
+      } else {
+        mdRows.push(`| ${label} | ${code} | ${type} |`);
+      }
+    }
+    mdRows.sort();
+    const mdTable = [mdTableHeader, ...mdRows].join('\n');
+    console.log(mdTable);
+    navigator.clipboard.writeText(mdTable);
+    let formatter = markdownTableFormatter();
+    console.log(formatter.formatTable(mdTable));
+  }
+})();
